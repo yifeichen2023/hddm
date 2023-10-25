41366c15f4625d51374071ce031a428020a44311
yifeichen123 <45802800+yifeichen123@users.noreply.github.com>
2023-10-25 13:29:44 -0700

    debug set up issues

--- setup.py
+++ setup.py
@@ -1,35 +1,34 @@
 from setuptools import setup
 from setuptools import Extension
-#from setuptools.dist import Distribution
-#Distribution().fetch_build_eggs(['Cython>=0.29', 'numpy>=1.20']) # necessary to allow cold install into empty environment / otherwise complains about lack of numpy
-import numpy as np
 
 try:
     from Cython.Build import cythonize
-    ext_modules = cythonize([
-                             Extension('wfpt', ['src/wfpt.pyx'], language='c++'), # uncomment for OSX: , extra_compile_args=['-stdlib=libc++'], extra_link_args=['-stdlib=libc++', "-mmacosx-version-min=10.9"]),
+    ext_modules = cythonize([Extension('wfpt', ['src/wfpt.pyx'], language='c++'), # uncomment for OSX: , extra_compile_args=['-stdlib=libc++'], extra_link_args=['-stdlib=libc++', "-mmacosx-version-min=10.9"]),
                              Extension('cdfdif_wrapper', ['src/cdfdif_wrapper.pyx', 'src/cdfdif.c']),
-                            ], 
-                            compiler_directives = {"language_level": "3"})
+                             Extension('data_simulators', ['src/cddm_data_simulation.pyx'], language='c++'),
+    ], compiler_directives = {"language_level": "3"})
 
 except ImportError:
-    ext_modules = [
-                   Extension('wfpt', ['src/wfpt.cpp'], language='c++'),
+    ext_modules = [Extension('wfpt', ['src/wfpt.cpp'], language='c++'),
                    Extension('cdfdif_wrapper', ['src/cdfdif_wrapper.c', 'src/cdfdif.c']),
-                   ]
+                   Extension('data_simulators', ['src/cddm_data_simulation.cpp'], language="c++")
+    ]
+
+import numpy as np
 
 setup(
-    name='HDDM',
-    version='0.9.8',
-    author='Thomas V. Wiecki, Imri Sofer, Michael J. Frank, Mads Lund Pedersen, Alexander Fengler, Lakshmi Govindarajan, Krishn Bera',
+    name='HDDM2',
+    version='0.9.0',
+    author='Thomas V. Wiecki, Imri Sofer, Michael J. Frank, Mads Lund Pedersen, Alexander Fengler, Lakshmi Govindarajan',
     author_email='thomas.wiecki@gmail.com',
-    url='http://github.com/hddm-devs/hddm',
-    packages=['hddm', 'hddm.tests', 'hddm.models', 'hddm.examples', 'hddm.torch', 'hddm.torch_models', 'hddm.simulators'],
-    package_data={'hddm':['examples/*.csv', 'examples/*.conf', 'examples/demo_HDDMnnRL/*.csv', 'torch_models/*', 'simulators/*']},
+    # url='http://github.com/hddm-devs/hddm',
+    url = 'http://github.com/jungsunyoo/hddm_twostep',
+    packages=['hddm', 'hddm.tests', 'hddm.models', 'hddm.examples', 'hddm.torch', 'hddm.torch_models', 'hddm.simulators'], # 'hddm.cnn', 'hddm.cnn_models', 'hddm.keras_models',
+    package_data={'hddm':['examples/*.csv', 'examples/*.conf', 'torch_models/*', 'simulators/*']}, # 'cnn_models/*/*'  'keras_models/*.h5',
     scripts=['scripts/hddm_demo.py'],
     description='HDDM is a python module that implements Hierarchical Bayesian estimation of Drift Diffusion Models.',
-    install_requires=['numpy >=1.20.0', 'scipy >= 1.6.1', 'cython >= 0.29.0', 'pandas >= 1.0.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24', 'cloudpickle >= 2.0.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3, < 3.0.0', 'arviz >= 0.11', 'ssms >= 0.2.0'],
-    setup_requires=['numpy >=1.20.0', 'scipy >= 1.6.1',  'cython >= 0.29.0', 'pandas >= 1.0.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24', 'cloudpickle >= 2.0.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3, < 3.0.0', 'arviz >= 0.11', 'ssms >= 0.2.0'],
+    install_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'pandas >= 0.12.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24', 'cloudpickle >= 2.0.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3'],
+    setup_requires=['NumPy >=1.6.0', 'SciPy >= 0.6.0', 'pandas >= 0.12.0', 'patsy', 'seaborn >= 0.11.0', 'statsmodels >= 0.12.0', 'tqdm >= 4.1.0', 'scikit-learn >= 0.24', 'cloudpickle >= 2.0.0', 'kabuki >= 0.6.0', 'PyMC >= 2.3.3'],
     include_dirs = [np.get_include()],
     classifiers=[
                 'Development Status :: 5 - Production/Stable',
